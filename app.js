63b55e3bccb3508732f203c3e3b3563e3875f741
Luke Tyler <luketyler17@gmail.com>
2022-09-06 11:23:29 -0400

    1

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 8080
 
+var cookieParser = require('cookie-parser')
+app.use(cookieParser("insertsecretKeyHere"))
+
+app.get('/login', (req, res) => {
+    res.status(200)
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
+
+req.cookieParser
+res.cookie(, {
+    signed: true,
+    secure: true,
+    httpOnly: true,
+})
+res.clearCookie()
+
+req.signedCookes()
+
+{
+    signed: true,
+    secure: true,
+    httpOnly: true,
+}
